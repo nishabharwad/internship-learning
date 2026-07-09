dff00dacd66b6f4d3f8c14b930141d6ebb266f1b
imtrynabuildshit <nishabharwad02@gmail.com>
2026-07-09 11:58:11 +0530

    added window

---
+++ extjs/extjs_beginner_demo/js/app.js
@@ -0,0 +1,33 @@
+Ext.onReady(function() {
+
+    var openBtn = new Ext.Button({
+        text: 'Open Window',
+        renderTo: Ext.getBody(),
+        handler: function() {
+            myWindow.show();
+        }
+    });
+
+    var myWindow = new Ext.Window({
+        title: 'window',
+        width: 300,
+        height: 200,
+        closable: true,
+        modal: true,
+        items: [
+            new Ext.Button({
+                text: 'ok',
+                handler: function() {
+                    Ext.MessageBox.alert('Message', 'OK button is clicked');
+                }
+            }),
+            new Ext.Button({
+                text: 'Close',
+                handler: function() {
+                    myWindow.close();
+                }
+            })
+        ]
+    });
+
+});
