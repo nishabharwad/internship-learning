79b9eccecef39e9d07086497f29b49f71ce9de75
imtrynabuildshit <nishabharwad02@gmail.com>
2026-07-31 10:53:18 +0530

    experimented a bit more

--- task-2 extjs/js/app.js
+++ task-2 extjs/js/app.js
@@ -4,7 +4,7 @@ Ext.onReady(function() {
         proxy: new Ext.data.HttpProxy({
             url: 'https://jsonplaceholder.typicode.com/users'
         }),
-        reader: new Ext.data.JsonReader({ fields: ['name', 'email', 'phone', 'website'] })
+        reader: new Ext.data.JsonReader({ fields: ['name', 'email', 'phone', 'website', { name: 'city', mapping: 'address.city' }] })
     });
 
     var userGrid = new Ext.grid.GridPanel({
@@ -14,10 +14,11 @@ Ext.onReady(function() {
             { header: 'Name',    dataIndex: 'name',    width: 150 },
             { header: 'Email',   dataIndex: 'email',   width: 200 },
             { header: 'Phone',   dataIndex: 'phone',   width: 150 },
-            { header: 'Website', dataIndex: 'website', width: 150 }
+            { header: 'Website', dataIndex: 'website', width: 150 },
+            { header: 'City',    dataIndex: 'city',    width: 150 }
         ],
         renderTo: 'grid-container',
-        width: 660,
+        width: 760,
         height: 280,
         loadMask: true,   
         stripeRows: true 
@@ -25,9 +26,10 @@ Ext.onReady(function() {
 
     userStore.load({
         callback: function(){
-
+           
             while (userStore.getCount()>6)
             {
+                 userStore.sort('name', 'DESC' )
                 userStore.removeAt(6);
             }
         }
